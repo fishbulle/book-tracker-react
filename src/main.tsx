56ea94fa823aaca652f246c9dd6407fb04d4535c
Angelina <linaemalmros@gmail.com>
2023-07-04 10:34:43 +0200

    changed structure of the project

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root from './routes/Root';
+import Root from './home/Root';
 import ErrorPage from './error-page';
 import "../src/assets/styles/style.css";
 
