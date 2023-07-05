8bee48c87c5910c3609291b090ec42805e652880
Angelina <linaemalmros@gmail.com>
2023-07-05 10:29:38 +0200

    added dummy data

---
+++ src/library/books.ts
@@ -0,0 +1,50 @@
+export const books = [
+    {
+        id: '1',
+        title: 'Harry Potter and the Prisoner of Azkaban',
+        author: 'J.K. Rowling',
+        blurb: "Harry Potter, along with his best friends, Ron and Hermione, is about to start his third year at Hogwarts School of Witchcraft and Wizardry. Harry can't wait to get back to school after the summer holidays. \n\n(Who wouldn't if they lived with the horrible Dursleys?) \n\nBut when Harry gets to Hogwarts, the atmosphere is tense. There's an escaped mass murderer on the loose, and the sinister prison guards of Azkaban have been called in to guard the school...",
+        cover: require('../assets/harrypotter.jpg'),
+        genre: 'childrens'
+    },
+    {
+        id: '2',
+        title: 'The Cruel Prince',
+        author: 'Holly Black',
+        blurb: "Jude was seven years old when her parents were murdered and she and her two sisters were stolen away to live in the treacherous High Court of Faerie. Ten years later, Jude wants nothing more than to belong there, despite her mortality. But many of the fey despise humans. Especially Prince Cardan, the youngest and wickedest son of the High King. \n\nTo win a place at the Court, she must defy him - and face the consequences. \n\nAs Jude becomes more deeply embroiled in palace intrigues and deceptions, she discovers her own capacity for trickery and bloodshed. But as betrayal threatens to drown the Courts of Faerie in violence, Jude will need to risk her life in a dangerous alliance to save her sisters, and Faerie itself.",
+        cover: require('../assets/cruelprince.jpg'),
+        genre: 'fantasy'
+    },
+    {
+        id: '3',
+        title: 'The Hating Game',
+        author: 'Sally Thorne',
+        blurb: "Nemesis (n.) \n 1) An opponent or rival whom a person cannot best or overcome.\n 2) A person's undoing. \n 3) Joshua Templeman.  \n\nLucy Hutton and Joshua Templeman hate each other. Not dislike. Not begrudgingly tolerate. Hate. And they have no problem displaying their feelings through a series of ritualistic passive aggressive maneuvers as they sit across from each other, executive assistants to co-CEOs of a publishing company. Lucy can’t understand Joshua’s joyless, uptight, meticulous approach to his job. Joshua is clearly baffled by Lucy’s overly bright clothes, quirkiness, and Pollyanna attitude. \n\nNow up for the same promotion, their battle of wills has come to a head and Lucy refuses to back down when their latest game could cost her her dream job…But the tension between Lucy and Joshua has also reached its boiling point, and Lucy is discovering that maybe she doesn’t hate Joshua.And maybe, he doesn’t hate her either.Or maybe this is just another game.",
+        cover: require('../assets/hatinggame.jpg'),
+        genre: 'romance'
+    },
+    {
+        id: '4',
+        title: 'Gild',
+        author: 'Raven Kennedy',
+        blurb: "The fae abandoned this world to us. And the ones with power rule. \n\nGold. \n\nGold floors, gold walls, gold furniture, gold clothes. In Highbell, in the castle built into the frozen mountains, everything is made of gold. \n\nEven me. \n\nKing Midas rescued me. Dug me out of the slums and placed me on a pedestal. I'm called his precious. His favored. I'm the woman he Gold-Touched to show everyone that I belong to him. To show how powerful he is. He gave me protection, and I gave him my heart. And even though I don't leave the confines of the palace, I'm safe. \n\nUntil war comes to the kingdom and a deal is struck. \n\nSuddenly, my trust is broken. My love is challenged. And I realize that everything I thought I knew about Midas might be wrong. \n\nBecause these bars I'm kept in, no matter how gilded, are still just a cage. But the monsters on the other side might make me wish I'd never left.",
+        cover: require('../assets/gild.jpg'),
+        genre: 'retellings'
+    },
+    {
+        id: '5',
+        title: 'The Picture of Dorian Grey',
+        author: 'Oscar Wilde',
+        blurb: "Oscar Wilde's only novel is the dreamlike story of a young man who sells his soul for eternal youth and beauty. In this celebrated work Wilde forged a devastating portrait of the effects of evil and debauchery on a young aesthete in late 19th century England. \n\nCombining elements of the Gothic horror novel and decadent French fiction, the book centers on a striking premise: As Dorian Gray sinks into a life of crime and gross sensuality, his body retains perfect youth and vigor while his recently painted portrait grows day by day into a hideous record of evil, which he must keep hidden from the world. \n\nFor over a century, this mesmerizing tale of horror and suspense has enjoyed wide popularity.It ranks as one of Wilde's most important creations and among the classic achievements of its kind.",
+        cover: require('../assets/doriangrey.jpg'),
+        genre: 'classics'
+    },
+    {
+        id: '6',
+        title: 'Icebreaker',
+        author: 'Hannah Grace',
+        blurb: "Anastasia Allen has worked her entire life for a shot at Team USA. \n\nA competitive figure skater since she was five years old, a full college scholarship thanks to her place on the Maple Hills skating team, and a schedule that would make even the most driven person weep, Stassie comes to win.\n\nNo exceptions.\n\nNathan Hawkins has never had a problem he couldn’t solve.As captain of the Maple Hills Titans, he knows the responsibility of keeping the hockey team on the ice rests on his shoulders.\n\nWhen a misunderstanding results in the two teams sharing a rink, and Anastasia’s partner gets hurt in the aftermath, Nate finds himself swapping his stick for tights, and one scary coach for an even scarier one.\n\nThe pair find themselves stuck together in more ways than one, but it’s fine, because Anastasia doesn’t even like hockey players…right?",
+        cover: require('../assets/icebreaker.jpg'),
+        genre: 'sports romance'
+    }
+]
